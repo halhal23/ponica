c1add31e7a910ac9445e9ef872c853c4c82dff8c
halhal <hh.25799623@gmail.com>
2021-12-03 13:06:42 +0900

    add layout for frontend

---
+++ front/components/Header.vue
@@ -0,0 +1,8 @@
+<template>
+  <header>
+    <form action="/search" method="get">
+      <input name="q" type="text" placeholder="キーワード" />
+      <button>検索</button>
+    </form>
+  </header>
+</template>
